4b537dda0788c5fb386252b4302240b5e08401ef
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-09-18 17:25:25 +0900

    [update] 列挙型の使用
    アニメーション名の指定に列挙型を利用するように修正。

--- Assets/Scripts/Player/PlayerAnimationHandler.cs
+++ Assets/Scripts/Player/PlayerAnimationHandler.cs
@@ -15,16 +15,31 @@ public class PlayerAnimationHandler : MonoBehaviour
         _formHandler = new PlayerFormHandler();
     }
 
+    //-------------------------------------------------------------------------------
+    // 形態を切り替えるアニメーション
+    //-------------------------------------------------------------------------------
+
     /// <summary>形態を切り替えるアニメーションを再生する</summary>
-    public void PlaySwitchFormAnimation()
+    public void PlaySwitchAnimation()
     {
-        if (_formHandler.GetPlayerForm() == PlayerForm.Robot)
-        {
-            _animator.Play("Robot Form");
-        }
-        else if (_formHandler.GetPlayerForm() == PlayerForm.Jett)
-        {
-            _animator.Play("Jett Form");
-        }
+        _animator.Play(IsRobot() ? PlayerAnimation.Jett.ToString() : PlayerAnimation.Robot.ToString());
+    }
+
+    //-------------------------------------------------------------------------------
+    // アニメーションの再生に関する処理
+    //-------------------------------------------------------------------------------
+
+    /// <summary>プレイヤーが歩行形態であるか</summary>
+    private bool IsRobot()
+    {
+        if (_formHandler.GetPlayerForm() == PlayerForm.Robot) return true;
+        else return false;
+    }
+
+    /// <summary>プレイヤーが飛行形態であるか</summary>
+    private bool IsJett()
+    {
+        if (_formHandler.GetPlayerForm() == PlayerForm.Jett) return true;
+        else return false;
     }
 }
