c510ad5e322da3e9b86470f73ed4264fc89dc7fa
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-09-18 16:53:21 +0900

    [add] 攻撃管理クラス

---
+++ Assets/Scripts/Player/PlayerAttackHandler.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+/// <summary>プレイヤーの攻撃処理を管理するクラス</summary>
+public class PlayerAttackHandler : MonoBehaviour
+{
+    /// <summary>近接攻撃</summary>
+    public void MeleeAttack()
+    {
+
+    }
+
+    /// <summary>遠距離攻撃</summary>
+    public void RangedAttack()
+    {
+
+    }
+}
