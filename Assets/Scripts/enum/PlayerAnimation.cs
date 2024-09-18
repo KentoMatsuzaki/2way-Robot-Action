a618fef40a1f4621d07243078725eda1182276f2
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-09-18 17:24:26 +0900

    [add] アニメーションを示す列挙型

---
+++ Assets/Scripts/enum/PlayerAnimation.cs
@@ -0,0 +1,8 @@
+/// <summary>プレイヤーのアニメーションを示す列挙型</summary>
+public enum PlayerAnimation
+{
+    Robot, // ロボット形態に切り替えるアニメーション
+    Jett, // ジェット形態に切り替えるアニメーション
+    RobotIdle, // ロボット形態のアイドルアニメーション
+    JettIdle, // ジェット形態のアイドルアニメーション
+}
