24de07521c1b801ad7cea500b2cb000c58d1caef
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-09-18 16:53:07 +0900

    [add] アニメーション管理クラス

---
+++ Assets/Scripts/Player/PlayerAnimationHandler.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+/// <summary>プレイヤーのアニメーションを管理するクラス</summary>
+public class PlayerAnimationHandler : MonoBehaviour
+{
+    /// <summary>プレイヤーのアニメーターコントローラー</summary>
+    Animator _animator;
+
+    /// <summary>プレイヤーの形態管理</summary>
+    PlayerFormHandler _formHandler;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _formHandler = new PlayerFormHandler();
+    }
+
+    /// <summary>形態を切り替えるアニメーションを再生する</summary>
+    public void PlaySwitchFormAnimation()
+    {
+        if (_formHandler.GetPlayerForm() == PlayerFormHandler.PlayerForm.Robot)
+        {
+            _animator.Play("Robot Form");
+        }
+        else if (_formHandler.GetPlayerForm() == PlayerFormHandler.PlayerForm.Jett)
+        {
+            _animator.Play("Jett Form");
+        }
+    }
+}
