78501dca5b1b8df522b3091b32eae26010b1a50c
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-10-02 16:00:02 +0900

    [add] アニメーションの種類の列挙型
    アニメーションの種類を示す列挙型を新調

--- Assets/Scripts/enum/PlayerAnimation.cs
+++
@@ -1,8 +0,0 @@
-/// <summary>プレイヤーのアニメーションを示す列挙型</summary>
-public enum PlayerAnimation
-{
-    Robot, // ロボット形態に切り替えるアニメーション
-    Jett, // ジェット形態に切り替えるアニメーション
-    RobotIdle, // ロボット形態のアイドルアニメーション
-    JettIdle, // ジェット形態のアイドルアニメーション
-}
---
+++ Assets/Scripts/enum/PlayerAnimationType.cs
@@ -0,0 +1,7 @@
+/// <summary>プレイヤーのアニメーションの種類を示す列挙型</summary>
+public enum PlayerAnimationType
+{
+    Switch, // 形態を切り替える
+    Idle, // アイドル
+    Move, // 移動
+}
