0885d783c045bb9e2e839463739534f9cddaeda3
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-10-06 22:06:44 +0900

    [add] 三人称カメラのクラス
    フィールドを実装

---
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+/// <summary>三人称視点カメラ</summary>
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    /// <summary>プレイヤーの位置</summary>
+    [SerializeField, Header("プレイヤーの位置")] private Transform _playerPos;
+
+    /// <summary>プレイヤーとの距離</summary>
+    [SerializeField, Header("プレイヤーとの距離")] private float _distance;
+
+    /// <summary>カメラ感度</summary>
+    [SerializeField, Header("カメラ感度")] private float _sensitivity;
+
+    /// <summary>Y軸の最小回転角度</summary>
+    [SerializeField, Header("Y軸の最小回転角度")] private float _minAngleY;
+
+    /// <summary>Y軸の最大回転角度</summary>
+    [SerializeField, Header("Y軸の最大回転角度")] private float _maxAngleY;
+
+    /// <summary>現在のX軸方向のマウス移動量</summary>
+    private float _currentX;
+
+    /// <summary>現在のY軸方向のマウス移動量</summary>
+    private float _currentY;
+
+    /// <summary>カメラのオフセット</summary>
+    private Vector3 _offset;
+}
