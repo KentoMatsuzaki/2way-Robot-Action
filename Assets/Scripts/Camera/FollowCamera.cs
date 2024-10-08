38c15be6ca5048b10e549bde8f671e21315129ea
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-10-08 12:28:07 +0900

    [update] 追跡カメラ
    プレイヤーの回転に対応させた。

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -7,22 +7,37 @@ public class FollowCamera : MonoBehaviour
     /// <summary>プレイヤーの位置</summary>
     [SerializeField, Header("プレイヤーの位置")] private Transform _player;
 
-    /// <summary>プレイヤーとの距離</summary>
-    [SerializeField, Header("プレイヤーとの距離")] private float _distance;
-
     /// <summary>カメラのオフセット</summary>
-    private Vector3 _offset;
+    [SerializeField, Header("カメラのオフセット")] private Vector3 _offset;
+
+    /// <summary>マウスの操作感度</summary>
+    private float _mouseSensitivity;
+
+    /// <summary>マウスの入力を受け取る変数</summary>
+    private float _mouseX;
 
     private void Start()
     {
-        // カメラの位置を初期設定する
-        _offset = new Vector3(0, 1.25f, -_distance);
+        // マウスの操作感度を取得する
+        _mouseSensitivity = _player.gameObject.GetComponent<PlayerRotateHandler>().MouseSensitivity;
+    }
+
+    private void Update()
+    {
+        // マウスの入力を受け取る
+        _mouseX += Input.GetAxis("Mouse X") * _mouseSensitivity;
     }
 
     private void LateUpdate()
     {
-        // カメラの位置をプレイヤーの背後に設定
-        transform.position = _player.position + _offset;
+        // 回転を計算する
+        Quaternion rotation = Quaternion.Euler(0, _mouseX, 0);
+
+        // カメラの新しい位置を計算する
+        Vector3 newPos = _player.position + rotation * _offset;
+
+        // カメラを新しい位置に移動させる
+        transform.position = newPos;
 
         // カメラが常にプレイヤーの背面を向くようにする
         transform.LookAt(new Vector3(_player.position.x, _player.position.y + 1.25f, _player.position.z));
